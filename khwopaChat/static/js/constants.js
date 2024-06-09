bb5238518d846ebe2bb8794a4a76553a9c18b0fb
Arun Shrestha <arunstha547@gmail.com>
2024-06-09 13:54:27 +0545

    fsdfds

--- khwopaChat/static/js/constants.js
+++ khwopaChat/static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "http://192.168.0.115:5005/webhooks/rest/webhook";
+const rasa_server_url = "http://192.168.137.197:5005/webhooks/rest/webhook";
 const sender_id = uuidv4();
